0253eb0f3601ec10b86d2cf09cb09c3d89369b06
Matthew Scribner <mattscribner45@gmail.com>
2022-03-01 10:00:11 -0700

    Corrected capitalization on url path

--- src/src/urls.py
+++ src/src/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('oddjobs/', include('OddJobs.urls')),
+    path('OddJobs/', include('OddJobs.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
