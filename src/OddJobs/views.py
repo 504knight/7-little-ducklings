6f463da961d9445e111afab38c224e8feff1f711
jsladea <jaaronslade@gmail.com>
2022-03-05 16:02:15 -0700

    began work

--- src/OddJobs/views.py
+++ src/OddJobs/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import User
+from django.http import Http404
 
 
 # Create your views here.
@@ -56,3 +57,32 @@ def delete_user(request):
         request.user.delete()
 
     return redirect('OddJobbs:index')
+
+#job-history
+def job_history_page(request):
+    print("unimplemented")
+
+def job_history_data(request):
+    current_user = request.user
+    context = {}
+    if 'start_date' not in request.GET or 'end_date' not in request.GET:
+        raise Http404("Missing required parameters - must have start_date and end_date")
+    else:
+        try:
+            job_history = JobHistory.get_job_history(request)
+        except:
+            raise Http404("Error obtaining job history")
+
+
+
+class JobHistory:
+
+    @staticmethod
+    def get_job_history(request):
+        current_user = request.user
+        start_date = request.Get['start_date']
+        end_date = request.Get['end_date']
+        return list(current_user.get_job_history(start_date, end_date))
+
+
+
