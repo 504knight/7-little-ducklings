3a2496e77369ee9f3ca0b57bcefcd0226d72553f
jsladea <jaaronslade@gmail.com>
2022-03-14 23:17:25 -0600

    Finished prototype of job history page

--- src/OddJobs/static/OddJobs/jobhistory.js
+++ src/OddJobs/static/OddJobs/jobhistory.js
@@ -1,10 +1,24 @@
-let jobHistoryUrl = "..."
+let jobHistoryUrl = `${window.location.origin}/job_history_listings`;
 
-var reloadJobListings = function(startDate, endDate){
-    fetch(jobHistoryUrl + "?" + startDate + "&" + endDate)
+var reloadJobListings = function(){
+    //add error handling
+    fetch(`${jobHistoryUrl}?start_date=${getStartDate()}&end_date=${getEndDate()}`)
     .then(function(response){
         let jobListingsHTML =  response.text();
         let listingsTable = document.querySelector("#history-table");
         listingsTable.innerHTML = jobListingsHTML;
     });
+}
+
+var getStartDate = function(){
+    let startDatePicker = document.querySelector("#start-date-input");
+    //should probably filter input and also check to make sure that the format returned by .value works as parameter for obtaining job listings
+    //if invalid alert user
+    return startDatePicker.value;
+}
+
+var getEndDate = function(){
+    let endDatePicker = document.querySelector("#end-date-input");
+    //filter in the same way as getStartDate
+    return endDatePicker.value;
 }
