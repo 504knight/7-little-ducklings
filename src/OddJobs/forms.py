edbbbc195c027d161a7907360af8ad3f80343049
Matthew Scribner <mattscribner45@gmail.com>
2022-04-14 10:15:52 -0600

    Finished User Form Validation

--- src/OddJobs/forms.py
+++ src/OddJobs/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
 class NewUserForm(forms.Form):
-    first_name = forms.CharField(label='First name', max_length=100)
-    last_name = forms.CharField(label='First name', max_length=100)
-    password = forms.PasswordInput()
+    username = forms.CharField(label='Username', max_length=100)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    fname = forms.CharField(label='First name', max_length=100)
+    lname = forms.CharField(label='Last name', max_length=100)
+    choices = [(0, 'Customer'), (1, 'Worker')]
+    type = forms.ChoiceField(widget=forms.RadioSelect, choices=choices)
