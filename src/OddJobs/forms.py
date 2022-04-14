c405f4b4044b1c3970a0f710c932f9768052ab69
Logan Ballard <logan.ballard@aggiemail.usu.edu>
2022-04-14 11:06:45 -0600

    Style new user form class

--- src/OddJobs/forms.py
+++ src/OddJobs/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 class NewUserForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
-    fname = forms.CharField(label='First name', max_length=100)
-    lname = forms.CharField(label='Last name', max_length=100)
+    username = forms.CharField(label='Username', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    fname = forms.CharField(label='First name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    lname = forms.CharField(label='Last name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     choices = [(0, 'Customer'), (1, 'Worker')]
-    type = forms.ChoiceField(widget=forms.RadioSelect, choices=choices)
+    type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=choices)
